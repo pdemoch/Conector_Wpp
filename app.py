bc5229950baa7e64f4adbb9b58c39a3a9a944377
Philipe Democh da Silva <pdemoch@gmail.com>
2025-05-09 11:59:54 -0300

    token

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def webhook():
     if request.method == 'GET':
         token = request.args.get('hub.verify_token')
         challenge = request.args.get('hub.challenge')
-        if token == 'EAAIZBEkh9ymIBOZC3PzlyTPrOZATZAUriNXK5tNMP63BBMSga51aEnTbygG87ZBNnwwx3zfAD0n60iEbUHR71F1sBOZBzjn1SsuGtuZC7aFOFPyoxxKpZC33mhXVWKOLrizCSPbcHJH6p3ybrOPMekgLLJRhlRqfSrqV5er92vL6mqqSqsbAUF30rP1SXGWA3yZBtUpgPAwdGDZB5xLmgvg8CZBiu5e':
+        if token == 'EAAIZBEkh9ymIBOyJGqYlP06nlOLOil5pYJkqcb0aZCKZAwzS2fSMyPdp2fM0qw9Kd4uouYUK3HGyjRZC7Ec0F4Yb0pm2lPOvFcPOawxnvYfDAvkXuVlAso4gITGYZBFjSACNbSYZB4X3yZCM2jyIhoj4ZA0z6mv8ZAZB9S6isdXXZAAGG2zrIc7v0yKBi5ZCK3e8iE564d9Id3v50mRn9tLvAW8EfqoZD':
             return challenge, 200
         return 'Token inválido', 403
 
